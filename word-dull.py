482e667145b9639b860a19d60b0ace62ceb5eae3
Tyler England <54315363+tyler-england@users.noreply.github.com>
2022-01-13 16:24:14 -0500

    Allowed duplicate-letter suggestions

--- word-dull.py
+++ word-dull.py
@@ -2,7 +2,7 @@ from collections import Counter
 from pathlib import Path
 
 path = str(Path(__file__).parents[0])
-pathdoc = path+"/words.txt"
+pathdoc = path + "/words.txt"
 
 with open(pathdoc) as wordfile:
     wordsall = [line.rstrip() for line in wordfile]
@@ -21,12 +21,12 @@ def pare(wordlist, guess):  # gets info about letters & pares down list
     lets = [let for let in guess]
     wrd = ""
     for let in lets:
-        wrd = wrd + "  "+let.upper()
+        wrd = wrd + "  " + let.upper()
     print("\nEnter the following for each letter in the word, separated by commas")
     print("Y - the letter is at the correct location in the word")
     print("N - the letter does not appear in the word")
     print("X - the letter is in the word, but not at that location")
-    feedback = input(wrd.strip()+"\n").lower()
+    feedback = input(wrd.strip() + "\n").lower()
     fdlst = [x.strip() for x in feedback.split(",")]
     for i in range(len(fdlst)):
         remlist = []
@@ -65,15 +65,15 @@ def suggest(wordlist, numwords):  # suggests the next input (max. letter usage?)
         # has letter, no dupes
         if not hasdupes(word) and word.find(toplets[0][0]) > -1:
             newwords.append(word)
-    if len(newwords) == 0:  # redo, allow dupes
+    if len(newwords) < numwords:  # redo, allow dupes
         for word in wordlist:
             if word.find(toplets[0][0]) > -1:  # has letter
                 newwords.append(word)
     i = 1
-    if len(newwords) < numwords+1:
+    if len(newwords) < numwords + 1:
         wordsout = newwords
     else:
-        while len(newwords) > numwords-1 and i < 10:
+        while len(newwords) > numwords - 1 and i < 10:
             wordsout = newwords
             remlist = []
             for word in newwords:
@@ -91,7 +91,7 @@ def suggest(wordlist, numwords):  # suggests the next input (max. letter usage?)
 
 
 sugg = suggest(words, 3)
-print("\n"+sugg)
+print("\n" + sugg)
 
 seed = input("\nEnter initial word guess: ").lower()
 words = pare(words, seed)
@@ -101,12 +101,15 @@ while i < 6 and len(words) > 1:
     attempt = "more"
     j = -2
     while attempt.lower() == "more":  # if more word suggestions are requested
-        j = j+3
+        j = j + 3
         sugg = suggest(words, j)
         attempt = input(
             "\nEnter next word guess or \"more\" (suggested: " + sugg.upper() + ")\n")
     words = pare(words, attempt)
     i += 1
 
-print("\nDone:")
-print(words[0])
+if len(words) > 0:
+    print("\nDone:")
+    print(words[0])
+else:
+    print("No more potential words")
